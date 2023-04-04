646f57f269f70af2f937734ff88063fbb4f05ba2
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-04 14:20:10 +0200

    add: completed exercise

---
+++ js/Javascript.js
@@ -0,0 +1,7 @@
+const name = (prompt('come ti chiami?'));
+
+const surname =(prompt('cognome?'))
+
+const color = (prompt('il tuo colore preferito?'))
+document.getElementById('user').innerHTML=(name + surname + color + '23')
+// document.writeln(name + surname + color + '23')
